4086f9ccb01183bae96e8030638dbc1fececd1e5
juniorperon <junior.peron@hotmail.com>
2022-09-22 02:12:39 -0300

    aula3

--- esports/server/src/server.ts
+++ esports/server/src/server.ts
@@ -1,38 +1,105 @@
 import express from 'express'
+import cors from 'cors'
+
 import { PrismaClient } from '@prisma/client'
+import { convertHourStringToMinute } from './utils/convert-hour-string-to-minute'
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hours-string'
 
 const app = express()
-const prisma = new PrismaClient()
 
-/*
-    - Query
-    - Route
-    - Body
-*/
+app.use(express.json())
+app.use(cors())
+
+const prisma = new PrismaClient({
+    log: ['query']
+})
 
 //mostrar games
-app.get('/games', (request, response) => {
-    return response.json([]);
+app.get('/games', async (request, response) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads:true,
+                }
+            }
+        }
+    })
+    
+    return response.json(games);
 });
 
 //criar ads
-app.post('/ads', (request, response) => {
-    return response.status(201).json([]);
+app.post('/games/:id/ads', async (request, response) => {
+    const gameId = request.params.id;
+    const body: any = request.body;
+
+    // validação                                                                                                                                  
+
+    const ad = await prisma.ad.create({
+        data: {
+            gameId,
+            name: body.name,
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays.join(','),
+            hourStart: convertHourStringToMinute(body.hourStart),
+            hourEnd: convertHourStringToMinute(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel,
+        }
+    })
+
+    return response.status(201).json(ad);
 });
 
 //mostrar ads
-app.get('/games/:id/ads', (request, response) => {
+app.get('/games/:id/ads', async (request, response) => {
     const gameId = request.params.id;
 
-    return response.send(gameId);
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart:true,
+            hourEnd: true,
+        },
+        where: {
+            gameId,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        }
+    })
 
+    return response.json(ads.map(ad => {
+        return {
+            ...ad,
+            weekDays: ad.weekDays.split(','),
+            hourStart: convertMinutesToHourString(ad.hourStart),
+            hourEnd: convertMinutesToHourString(ad.hourEnd),
+        }
+    }))
 })
 
 //mostrar discord
-app.get('/ads/:id/discord', (request, response) => {
+app.get('/ads/:id/discord', async (request, response) => {
     const adId = request.params.id;
 
-    return response.send(adId);
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+            discord: true,
+        },
+        where: {
+            id: adId,
+        }
+    })
+
+    return response.json({
+        discord: ad.discord,
+    });
 
 })
 
---
+++ esports/server/src/utils/convert-hour-string-to-minute.ts
@@ -0,0 +1,10 @@
+// user  ->    split     ->  number  -> calculos
+// 18:00 -> ["18", "00"] -> [18, 00] -> 1080
+
+export function convertHourStringToMinute(hourString: string) {
+    const [hours, minutes] = hourString.split(':').map(Number)
+
+    const minutesAmount = (hours*60) + minutes;
+
+    return minutesAmount;
+}
